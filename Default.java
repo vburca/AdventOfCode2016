bf85adb0ac2bb299f54309aeb7e6c62dcc32bb3e
Vlad Burca <vladburca2005@gmail.com>
2016-12-11 22:30:49 -0800

    Add default class for building solutions

---
+++ Default.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+
+public class Default {
+
+  public static void main( String[] args ) {
+    String fileName = args[0];
+
+    try {
+      FileReader reader = new FileReader( fileName );
+      BufferedReader bufferedReader = new BufferedReader( reader );
+
+      String line;
+
+      while ( ( line = bufferedReader.readLine() ) != null ) {
+      }
+
+      reader.close();
+    } catch ( IOException e ) {
+      e.printStackTrace();
+    }
+  }
+}
